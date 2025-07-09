0c257a8be2cae6dcf5e3db1163dc7717bd65eb2d
Alexandre Santos <alex.crispa02@gmail.com>
2025-07-09 15:14:59 -0300

    feat: update Home component layout to include AdminButton and FeedbackButton

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
+import FeedbackButton from "./FeedbackButton";
+import AdminButton from "./AdminButton";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-background p-4">
-      <main className="flex flex-col gap-8 items-center w-full max-w-md bg-card rounded-2xl shadow-2xl p-8 border border-card-border">
+      <main className="flex flex-col gap-8 items-center w-full max-w-md bg-card rounded-2xl shadow-2xl p-8 border border-card-border relative">
+        <div className="w-full flex justify-end mb-2">
+          <AdminButton />
+        </div>
         <Image
           className="white:invert mb-2"
           src="/assets/logo-removebg-preview.png"
@@ -16,6 +21,7 @@ export default function Home() {
         <p className="text-foreground/80 text-lg text-center">Aqui será o seu feed estilo "Swipe" para gamers.<br/>(Em breve!)</p>
         <p className="text-foreground/80 text-lg text-center">Precisamos de mais usuários para enfim lançarmos o aplicativo</p>
         <p className="text-foreground/80 text-lg text-center">Logo na playstore!</p>
+        <FeedbackButton />
       </main>
     </div>
   );
